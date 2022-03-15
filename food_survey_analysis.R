1d8ea9d9e4eb73c9d9a52f575d0c7c8c37d605f2
chrisgalgojr <chris.galgo1@gmail.com>
2022-03-15 12:44:14 +0800

    updated code

--- food_survey_analysis.R
+++ food_survey_analysis.R
@@ -10,139 +10,79 @@ install.packages("quanteda")
 install.packages("dplyr")
 install.packages("tm")
 install.packages("writexl")
+install.packages("forcats")
 
-##load packages
+##load packages----
 
 library("readxl")
 library("quanteda")
-
 library(ggplot2)
 library(tidyverse)
 library(writexl)
 
-
-
 ##read data----
 data<-read_excel("food_survey.xlsx")
 
-corp=corpus(data, text_field = 'healthy_food') #create the corpus
-corp
+#Text analysis
+#Creating corpus
+corp1=corpus(data, text_field = 'healthy_food') #create the corpus_healthyfood
+corp2=corpus(data, text_field = 'unhealthy_food') #create the corpus_unhealthyfood
+corp3=corpus(data, text_field = 'future_food') #create the corpus_futurefood
+corp4=corpus(data, text_field = 'facilitate_food_future') #create the corpus_fff
+corp5=corpus(data, text_field = 'inhibit_food_future') #create the corpus_inhibitff
+corp6=corpus(data, text_field = 'info_source') #create the corpus_info_source
+corp7=corpus(data, text_field = 'consumption_covid') #create the corpus_consumption_covid
 
-dtm=dfm(corp, stem=T, remove = stopwords('en'), remove_punct=T)
-dtm #sparse = of all the cells contain the value 0
+#Creating DTM
+dtm1=dfm(corp1, stem=T, remove = stopwords('en'), remove_punct=T)
+dtm2=dfm(corp2, stem=T, remove = stopwords('en'), remove_punct=T)
+dtm3=dfm(corp3, stem=T, remove = stopwords('en'), remove_punct=T)
+dtm4=dfm(corp4, stem=T, remove = stopwords('en'), remove_punct=T)
+dtm5=dfm(corp5, stem=T, remove = stopwords('en'), remove_punct=T)
+dtm6=dfm(corp6, stem=T, remove = stopwords('en'), remove_punct=T)
+dtm7=dfm(corp7, stem=T, remove = stopwords('en'), remove_punct=T) #sparse = of all the cells contain the value 0
 
-dtm <- dfm_trim(dtm, min_termfreq = 5)
-dtm
+#dtm1 <- dfm_trim(dtm1, min_termfreq = 5)
+#textplot_wordcloud(dtm, max_words = 50)                          ## top 50 (most frequent) words
+textplot_wordcloud(dtm1, max_words = 50, color = c('blue','red')) ## change colors
+textplot_wordcloud(dtm2, max_words = 50, color = c('blue','red')) ## change colors
+textplot_wordcloud(dtm3, max_words = 50, color = c('blue','red')) ## change colors
+textplot_wordcloud(dtm4, max_words = 50, color = c('blue','red')) ## change colors
+textplot_wordcloud(dtm5, max_words = 50, color = c('blue','red')) ## change colors
+textplot_wordcloud(dtm6, max_words = 50, color = c('blue','red')) ## change colors
+textplot_wordcloud(dtm7, max_words = 50, color = c('blue','red')) ## change colors
 
-textplot_wordcloud(dtm, max_words = 50)                          ## top 50 (most frequent) words
-textplot_wordcloud(dtm, max_words = 50, color = c('blue','red')) ## change colors
-textstat_frequency(dtm, n = 10)
+##
+textstat_frequency(dtm1, n = 30)
+textstat_frequency(dtm2, n = 20)
+textstat_frequency(dtm3, n = 20)
+textstat_frequency(dtm4, n = 10)
+textstat_frequency(dtm5, n = 10)
+textstat_frequency(dtm6, n = 10)
+textstat_frequency(dtm7, n = 10)
 
-male <- docvars(dtm)$sex == 'Male' 
-male_dtm <- dtm[male,]
-textplot_wordcloud(male_dtm, max_words = 25)
-textstat_frequency(male_dtm, n = 10)
+##Plotting DTM
+dataframe1 <- as.data.frame(colSums(as.matrix(dtm1)))
+dataframe1 <-rownames_to_column(dataframe1)
+colnames(dataframe1) <- c("Feature", "Frequency")
+dataframe1 <- arrange(dataframe1, desc(Frequency))
+dataframe1 <- dataframe1[-c(3,6,11),]
+healthyfood <- subset(dataframe1, Frequency >=5)
+ggplot(healthyfood,aes(x=reorder(Feature,Frequency), y=Frequency))+
+  geom_col() + 
+  coord_flip()+
+  xlab("Feature") + ylab("Frequency")
 
-
-female <- docvars(dtm)$sex == 'Female' 
-female_dtm <- dtm[female,]
-textplot_wordcloud(female_dtm, max_words = 25)
-textstat_frequency(female_dtm, n = 10)
-
-
-##Data frame for DTM
-sums1 <- as.data.frame(colSums(as.matrix(DTM1)))
-sums1<-rownames_to_column(sums1)
-colnames(sums1) <- c("term", "count")
-sums1 <- arrange(sums1, desc(count))
-#sums1 <- subset(sums1, count >=10)
-write_xlsx(sums1,"iCloud Drive\\Desktop\\dsprojects\\healthyfood.xlsx")
-
-
-
-class(sums1)
-
-sums2 <- as.data.frame(colSums(as.matrix(DTM2)))
-sums2<-rownames_to_column(sums2)
-colnames(sums2) <- c("term", "count")
-sums2 <- arrange(sums2, desc(count))
-sums2 <- subset(sums2, count >=10)
-
-sums3 <- as.data.frame(colSums(as.matrix(DTM3)))
-sums3<-rownames_to_column(sums3)
-colnames(sums3) <- c("term", "count")
-sums3 <- arrange(sums3, desc(count))
-sums3 <- subset(sums3, count >=10)
-
-sums3 <- as.data.frame(colSums(as.matrix(DTM3)))
-sums3 <-rownames_to_column(sums3)
-colnames(sums3) <- c("term", "count")
-sums3 <- arrange(sums3, desc(count))
-sums3 <- subset(sums3, count >=10)
-
-
-sums4 <- as.data.frame(colSums(as.matrix(DTM4)))
-sums4<-rownames_to_column(sums4)
-colnames(sums4) <- c("term", "count")
-sums4 <- arrange(sums4, desc(count))
-sums4 <- subset(sums4, count >=10)
+male <- docvars(dtm1)$sex == 'Male' 
+male_dtm <- dtm1[male,]
+#textplot_wordcloud(male_dtm, max_words = 25)
+textstat_frequency(male_dtm, n = 20)
 
 
-sums5 <- as.data.frame(colSums(as.matrix(DTM5)))
-sums5<-rownames_to_column(sums5)
-colnames(sums5) <- c("term", "count")
-sums5 <- arrange(sums5, desc(count))
-sums5 <- subset(sums5, count >=10)
-
-
-sums6 <- as.data.frame(colSums(as.matrix(DTM6)))
-sums6<-rownames_to_column(sums6)
-colnames(sums6) <- c("term", "count")
-sums6 <- arrange(sums6, desc(count))
-sums6 <- subset(sums6, count >=10)
-
-sums7 <- as.data.frame(colSums(as.matrix(DTM7)))
-sums7<-rownames_to_column(sums7)
-colnames(sums7) <- c("term", "count")
-sums7 <- arrange(sums7, desc(count))
-sums7 <- subset(sums7, count >=10)
-
-
-
-##Plotting healthy food
-ggplot(sums1,aes(x=factor(term), y=count))+
-  geom_col()+
-  coord_flip()
-
-ggplot(sums2,aes(x=factor(term), y=count))+
-  geom_col()+
-  coord_flip()
- 
-ggplot(sums3,aes(x=factor(term), y=count))+
-  geom_col()+
-  coord_flip()
-
-ggplot(sums4,aes(x=factor(term), y=count))+
-  geom_col()+
-  coord_flip()
-
-ggplot(sums5,aes(x=factor(term), y=count))+
-  geom_col()+
-  coord_flip()
-
-ggplot(sums6,aes(x=factor(term), y=count))+
-  geom_col()+
-  coord_flip()
-
-ggplot(sums7,aes(x=factor(term), y=count))+
-  geom_col()+
-  coord_flip()
-
-
-
-
-
-
+female <- docvars(dtm1)$sex == 'Female' 
+female_dtm <- dtm1[female,]
+#textplot_wordcloud(female_dtm, max_words = 25)
+textstat_frequency(female_dtm, n = 20)
 
 
 
